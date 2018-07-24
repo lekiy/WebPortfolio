722415cdbb6d1125303d77c3d7827e08ba0febf1
Kevin Annen <lekiyminin@gmail.com>
2018-07-24 17:33:23 -0500

    Not sure what changes

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
 
-function testJavascript(){
-    document.getElementById("javascript").innerHTML = "hello this is javascript";
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+var defaultBlogDisplay = "front-end-web";
+var currentBlogDisplay = defaultBlogDisplay;
+
+var projectData = {"front-end-web":"webproject1"};
+function changeBlogDisplay(displayGroup){
+    currentBlogDisplay = displayGroup;
+    displayProjects();
+    CallReact();
+}
+
+function displayProjects(){
+    var displayArea = document.getElementById("blog");
+    displayArea.innerHTML = "lol";
+}
+
+class HelloReact extends HelloReact.Component {
+    render(){
+        return React.createElement("div", null, "Hello React!");
+    }
+}
+
+function CallReact(){
+    ReactDOM.render(<HelloReact />, document.getElementById('root'));
 }
