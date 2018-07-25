4f3460aa5fb993d58c35ccb0ca03c666d440784e
Kevin Annen <lekiyminin@gmail.com>
2018-07-24 21:19:34 -0500

    Deleted things :,(

--- src/components/ContentButton.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-export default class ContentButton extends Component {
-    render(){
-        return (
-            <input type="button" className="NavButton ContentButton" onClick={this.props.onClick} value={this.props.name}/>
-        );
-    }
-}
